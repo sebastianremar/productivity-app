798aa827851608396d84fdbf7c3682d6edbfbc65
sebastianremar <rsrdlf93@gmail.com>
2023-02-28 23:40:18 -0500

    Fixed show content function

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var $content = $('.sidebar-menu');
-
-function showContent(selector) {
-  $content.hide();
-  $(selector).show();
+//  TO:DO fix this function to show only tab that has been clicked
+function showContent(contentId) {
+    // Hide all content divs
+    let contents = document.getElementsByClassName("sidebar-menu");
+    for (let i = 0; i < contents.length; i++) {
+      contents[i].style.display = "none";
+    }
+    
+    // Show the selected content div
+    let selectedContent = document.getElementById(contentId);
+    selectedContent.style.display = "block";
 }
-
-$('.sidebar').on('click', '.sidebar-menu', function(e) {
-  showContent(e.currentTarget.hash);
-  e.preventDefault();
-}); 
-
-// show '#about' content only on page load (if you want)
-showContent('#management');
